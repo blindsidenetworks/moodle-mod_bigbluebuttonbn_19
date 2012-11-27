04ce9fee7b42077d2626aa54b4d1d89a0a70d50d
jfederico <jesus@123it.ca>
2012-11-27 16:43:47 -0500

    v1.0.8a1: Removed ajax.php and bbb-broker.php, added ping.php - ajax.php is no longer needed due the actualization of recordingsbn - bbb-broker.php is also not required. the action ping can be simplified. - ping.php simplifies the action ping that was in the bbb-broker.php

---
+++ ping.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * View and administrate BigBlueButton playback recordings
+ *
+ * Authors:
+ *    Jesus Federico  (jesus [at] blindsidenetworks [dt] com)    
+ *
+ * @package   mod_bigbluebuttonbn
+ * @copyright 2012 Blindside Networks Inc.
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v2 or later
+ */
+
+require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
+require_once(dirname(__FILE__).'/locallib.php');
+
+require_login($course, true);
+
+$coursecontext = get_context_instance(CONTEXT_COURSE, $course->id);
+$moderator = has_capability('mod/bigbluebuttonbn:moderate', $coursecontext);
+
+$url = trim(trim($CFG->bigbluebuttonbnServerURL),'/').'/';
+$salt = trim($CFG->bigbluebuttonbnSecuritySalt);
+
+$meetingID = optional_param('meetingID', 0, PARAM_TEXT);
+if( $meetingID ){
+    echo bigbluebuttonbn_getMeetingXML( $meetingID, $url, $salt );
+} else {
+    echo 'false';
+}
+
